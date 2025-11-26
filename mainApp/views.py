62741f39575b98fcd643bfeee29890f322b67b2f
Lizbeth Calizaya <calizaya.lizbeth@gmail.com>
2025-11-26 01:55:54 -0300

    docs: agregar comentario descriptivo en vista del catálogo

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,5 @@
+# Vista principal del catálogo de productos - comentario aclaratorio
+
 from django.shortcuts import render
 from django.shortcuts import render,get_object_or_404
 from .models import Producto
