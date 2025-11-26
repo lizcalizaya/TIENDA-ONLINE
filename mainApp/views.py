53daf68a765b8a139c3d6725d88e88af23ecfd29
Lizbeth Calizaya <calizaya.lizbeth@gmail.com>
2025-11-26 01:49:29 -0300

    add: vistas catálogo y detalle de producto

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Producto
 
-# Create your views here.
+def catalogo(request):
+    productos = Producto.objects.all()
+    return render(request, 'catalogo.html', {
+        'productos': productos
+    })
+
+
+
+def producto_detalle(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    return render(request, 'producto_detalle.html', {
+        'producto': producto
+    })
