091036df5090548785ff33b63e0d524d3825d5d4
Lizbeth Calizaya <calizaya.lizbeth@gmail.com>
2025-12-01 01:35:48 -0300

    feat: agregar vista y ruta de seguimiento

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,12 +2,29 @@
 
 from django.shortcuts import render
 from django.shortcuts import render,get_object_or_404
-from .models import Producto
+from .models import Producto, Categoria, Pedido
+from .forms import PedidoForm
+
 
 def catalogo(request):
-    productos = Producto.objects.all()
+
+    categorias = Categoria.objects.all()
+
+    
+    categoria_id = request.GET.get('categoria')
+    if categoria_id:
+        productos = Producto.objects.filter(categoria_id=categoria_id)
+    else:
+        productos = Producto.objects.all()
+
+   
+    q = request.GET.get('q')
+    if q:
+        productos = productos.filter(nombre__icontains=q)
+
     return render(request, 'catalogo.html', {
-        'productos': productos
+        'productos': productos,
+        'categorias': categorias
     })
 
 
@@ -16,4 +33,34 @@ def producto_detalle(request, id):
     producto = get_object_or_404(Producto, id=id)
     return render(request, 'producto_detalle.html', {
         'producto': producto
+    })
+
+def crear_pedido(request, producto_id=None):
+    producto = None
+    if producto_id:
+        producto = Producto.objects.get(id=producto_id)
+
+    if request.method == 'POST':
+        form = PedidoForm(request.POST, request.FILES)
+        if form.is_valid():
+            pedido = form.save(commit=False)
+            pedido.plataforma = "Página Web"
+            pedido.producto = producto
+            pedido.save()
+            return render(request, 'pedido_confirmado.html', {'pedido': pedido})
+    else:
+        form = PedidoForm()
+
+    return render(request, 'crear_pedido.html', {
+        'form': form,
+        'producto': producto
+    })
+def seguimiento(request, token):
+    pedido = Pedido.objects.filter(token=token).first()
+
+    if not pedido:
+        return render(request, 'seguimiento_no_encontrado.html')
+
+    return render(request, 'seguimiento.html', {
+        'pedido': pedido
     })
--- tienda/urls.py
+++ tienda/urls.py
@@ -9,7 +9,10 @@ from mainApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.catalogo, name='catalogo'),
+    path('pedido/', views.crear_pedido, name='pedido_crear'),
     path('producto/<int:id>/', views.producto_detalle, name='producto_detalle'),
+    path('seguimiento/<uuid:token>/', views.seguimiento, name='seguimiento'),
+    path('pedido/<int:producto_id>/', views.crear_pedido, name='pedido_producto'),
 ]
 
 if settings.DEBUG:
