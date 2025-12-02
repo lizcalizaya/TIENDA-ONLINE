b4d9337ad3d25165298ff97119083e68d70214d3
Lizbeth Calizaya <calizaya.lizbeth@gmail.com>
2025-12-02 03:46:04 -0300

    feat: admin profesional actualizado

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.utils.html import format_html  
 
-from django.contrib import admin
 from .models import Categoria, Producto, Insumo, Pedido, PedidoImagenes
+
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
@@ -11,9 +12,15 @@ class CategoriaAdmin(admin.ModelAdmin):
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'categoria', 'precio_base', 'destacado')
-    list_filter = ('categoria','destacado')
+   
+    list_display = ('id', 'nombre', 'categoria', 'precio_base', 'destacado', 'imagen_principal')
+    list_filter = ('categoria', 'destacado')
     search_fields = ('nombre', 'descripcion')
+
+    def imagen_principal(self, obj):
+        if obj.imagen1:
+            return format_html('<img src="{}" width="50" height="50" style="object-fit: cover; border-radius: 5px;" />', obj.imagen1.url)
+        return "No Image"
 
 @admin.register(Insumo)
 class InsumoAdmin(admin.ModelAdmin):
@@ -21,15 +28,25 @@ class InsumoAdmin(admin.ModelAdmin):
     list_filter = ('tipo', 'marca', 'color')
     search_fields = ('nombre', 'tipo', 'marca', 'color')
 
+
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre_cliente', 'producto', 'plataforma', 'estado', 'pago', 'fecha_necesidad', 'fecha_creado')
+    list_display = (
+        'id',
+        'nombre_cliente',
+        'producto',
+        'estado',
+        'pago',
+        'plataforma',
+        'fecha_necesidad',
+        'fecha_creado',
+        'token',        
+    )
     list_filter = ('plataforma', 'estado', 'pago')
     search_fields = ('nombre_cliente', 'email', 'telefono', 'descripcion')
     readonly_fields = ('token', 'fecha_creado')
-       
+
 
 @admin.register(PedidoImagenes)
 class PedidoImagenesAdmin(admin.ModelAdmin):
     list_display = ('id', 'pedido', 'imagen')
-
